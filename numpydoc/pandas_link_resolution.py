eee10999badef140c4d1694d3cb5334da39ca023
Viraj <vnavkal0@gmail.com>
2018-02-07 17:12:39 -0800

    linkcode strategies

---
+++ numpydoc/pandas_link_resolution.py
@@ -0,0 +1,46 @@
+def linkcode_resolve(domain, info):
+    """
+    Determine the URL corresponding to Python object
+    """
+    if domain != 'py':
+        return None
+
+    modname = info['module']
+    fullname = info['fullname']
+
+    submod = sys.modules.get(modname)
+    if submod is None:
+        return None
+
+    obj = submod
+    for part in fullname.split('.'):
+        try:
+            obj = getattr(obj, part)
+        except:
+            return None
+
+    try:
+        fn = inspect.getsourcefile(obj)
+    except:
+        fn = None
+    if not fn:
+        return None
+
+    try:
+        source, lineno = inspect.getsourcelines(obj)
+    except:
+        lineno = None
+
+    if lineno:
+        linespec = "#L%d-L%d" % (lineno, lineno + len(source) - 1)
+    else:
+        linespec = ""
+
+    fn = os.path.relpath(fn, start=os.path.dirname(pandas.__file__))
+
+    if '+' in pandas.__version__:
+        return "http://github.com/pandas-dev/pandas/blob/master/pandas/%s%s" % (
+            fn, linespec)
+    else:
+        return "http://github.com/pandas-dev/pandas/blob/v%s/pandas/%s%s" % (
+            pandas.__version__, fn, linespec)
